cf99e6960422b46398911553f09a16b1d99f908f
2733707740@qq.com <2733707740@qq.com>
2023-02-16 21:37:39 +0800

    修复透明生成失败问题

--- CreateOpacityImageByOpenCVSharp/CreateOpacityImageByOpenCVSharp/MainWindow.xaml.cs
+++ CreateOpacityImageByOpenCVSharp/CreateOpacityImageByOpenCVSharp/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using OpenCvSharp;
+using OpenCvSharp.Extensions;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +21,7 @@ namespace CreateOpacityImageByOpenCVSharp
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : System.Windows.Window
     {
         public MainWindow()
         {
@@ -29,6 +31,8 @@ namespace CreateOpacityImageByOpenCVSharp
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Mat srcImage = CreateAlphaMat();
+            image.Source = BitmapToImageSource(BitmapConverter.ToBitmap(srcImage));
+            Cv2.ImWrite("透明Alpha.png", srcImage);
         }
 
         private Mat CreateAlphaMat()
@@ -38,10 +42,31 @@ namespace CreateOpacityImageByOpenCVSharp
             {
                 for (int j = 0; j < mat.Cols; j++) // 图像高度
                 {
-
+                    var rgba = new Vec4b();
+                    // 蓝色
+                    rgba.Item0 = 0xff;
+                    // 绿色
+                    rgba.Item1 = (byte)(((float)mat.Cols - j) / (float)mat.Cols * 0xff);
+                    // 红色
+                    rgba.Item2 = (byte)(((float)mat.Rows - i) / (float)mat.Rows * 0xff);
+                    // 透明度
+                    rgba.Item3 = (byte)((float)0.5 * (float)(rgba[1] + rgba[2]));
+                    // 设置
+                    mat.Set(i, j, rgba);
                 }
             }
             return mat;
+        }
+
+        public ImageSource BitmapToImageSource(Bitmap bitmap)
+        {
+            IntPtr hBitmap = bitmap.GetHbitmap();
+            ImageSource imageSource = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                hBitmap,
+                IntPtr.Zero,
+                Int32Rect.Empty,
+                BitmapSizeOptions.FromEmptyOptions());
+            return imageSource;
         }
     }
 }
